918e59826d0678dc3334888f96d1eea70f434d2c
Umar Sharif <umar.sharif@remotephpdevelopers.com>
2020-06-11 13:30:04 +0500

    Genetic Service class has been updated, write your custom code in genetic.service.ts file

--- src/app/services/genetic.service.ts
+++ src/app/services/genetic.service.ts
@@ -12,27 +12,94 @@ export class GeneticService {
 
   subscription: Subscription;
 
+  genetic:Genetic;
+
   constructor(private engServ: EngineService, private MS: MessageService, private FS: FunctionService) {
     // subscribe to component messages
     this.subscription = this.MS.getMessage().subscribe(message => {
       
       if (message == 'start_ga_click') {
-         this.start();
+         this.createGenetic();
+         this.startGenetic();
       }
 
     });
 
   }
 
-    start() {
-    
-        var genetic = Genetic.create();;
-    
-        
-    
-    console.log(genetic)
-    }
+  startGenetic() {
 
+    const config = {
+      "size": 250, //Population size
+      "crossover": 0.3, //Probability of crossover
+      "mutation": 0.3, //Probability of mutation
+      "iterations": 4000, //Maximum number of iterations before finishing
+      "fittestAlwaysSurvives": true, //Prevents losing the best fit between generations
+      "maxResults": 100, //The maximum number of best-fit results that webworkers will send per notification
+      "webWorkers": true, //Use Web Workers (when available)
+      "skip": 20 //Setting this higher throttles back how frequently genetic.notification gets called in the main thread.
+		};
+
+		const userData = {
+			
+		};
+
+    this.genetic.evolve( config, userData );
+    
+  }
+
+  createGenetic() {
+    
+        let genetic = Genetic.create();
+
+        genetic.optimize = Genetic.Optimize.Maximize;
+        
+        genetic.select1 = Genetic.Select1.Tournament2;
+        genetic.select2 = Genetic.Select2.Tournament2;
+
+        genetic.seed = function():any {
+
+          let  individual:any;
+
+          return individual;
+        };
+
+        genetic.fitness = function( individual ):number {
+          
+          let  fitness:number;
+
+          return fitness;
+        };
+
+        genetic.mutate = function( individual ):any {
+          
+          let  _individual:any;
+
+          return _individual;          
+        };
+
+        genetic.crossover = function( mother, father ):any {
+
+          let son:any;
+          let daughter:any;
+
+          return [ son, daughter ];     
+        };
+
+        genetic.generation = function( pop, generation, stats ):boolean {
+
+           
+          return false;
+        };
+
+        genetic.notification = function( pop, generation, stats, isFinished ) {
+
+
+        };
+    
+        this.genetic = genetic;
+  }
+    
 
   
 }
