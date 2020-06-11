bb75cf688022be45357d78cb93e2b153d20e3861
Umar Sharif <umar.sharif@remotephpdevelopers.com>
2020-06-11 19:07:04 +0500

    navigation and footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxNavbarModule } from 'ngx-bootstrap-navbar';
+
 import { AppComponent } from './app.component';
 import { EngineComponent } from './engine/engine.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EngineComponent,
     NavbarComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgxNavbarModule
   ],
   providers: [
   ],
